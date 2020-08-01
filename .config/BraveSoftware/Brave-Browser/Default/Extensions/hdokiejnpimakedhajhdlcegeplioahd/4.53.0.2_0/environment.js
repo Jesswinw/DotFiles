b745fd46389a8d59e9fc6b501e011d33819facaf
jesswinw <jesswinw@gmail.com>
2020-08-01 18:44:41 +0530

    removed unnecessary files

--- .config/BraveSoftware/Brave-Browser/Default/Extensions/hdokiejnpimakedhajhdlcegeplioahd/4.53.0.2_0/environment.js
+++
@@ -1,1 +0,0 @@
-var Environment={version:"4.53.0.2",environment:"prod",lastpass_url:"https://lastpass.com/"};
--- .config/BraveSoftware/Brave-Browser/Default/Extensions/hdokiejnpimakedhajhdlcegeplioahd/4.53.0.2_0/static/js/pbkdf2-ie11.chunk.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{575:function(n,o){},577:function(n,o){},608:function(n,o){},609:function(n,o){}}]);
